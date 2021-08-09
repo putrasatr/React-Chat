4a38ee0abf5d83bca92fac517471fafba22fe11d
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-09 18:53:47 +0700

    test static url

--- client-react-chat/src/actions/connect.js
+++ client-react-chat/src/actions/connect.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://192.168.1.58:3000/api',
     timeout: 1000,
     headers: { "X-Custom-Header": "foobar" }
 });
