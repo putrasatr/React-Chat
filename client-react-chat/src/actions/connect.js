f4ee996dd0936b8b3be0bfaa3ceea3970becd43a
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-17 15:35:39 +0700

    modify base url

--- client-react-chat/src/actions/connect.js
+++ client-react-chat/src/actions/connect.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: 'http://192.168.1.58:3000/api',
+    baseURL: 'http://localhost:3000/api',
     timeout: 1000,
     headers: {
-        "Authentication": `Bearer ${localStorage.getItem("token")}`,
         "X-Custom-Header": "foobar"
     }
 });
