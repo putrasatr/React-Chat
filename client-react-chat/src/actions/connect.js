7b047cd341666aae96eae7ca1ef2c3bb3d3c2ee8
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-17 15:06:21 +0700

    Instant Axios

--- client-react-chat/src/actions/connect.js
+++ client-react-chat/src/actions/connect.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 const request = axios.create({
     baseURL: 'http://192.168.1.58:3000/api',
     timeout: 1000,
-    headers: { "X-Custom-Header": "foobar" }
+    headers: {
+        "Authentication": `Bearer ${localStorage.getItem("token")}`,
+        "X-Custom-Header": "foobar"
+    }
 });
 export default request
